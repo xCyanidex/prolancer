dd79c1e2c56d64aa455fa0567fa2fc337eb88417
Duraiz <Haiderduraiz@gmail.com>
2022-10-27 00:35:16 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,5 @@
+const tab = document.querySelector(".tab");
+// const show = document.querySelector(".show");
+const tabcustom = document.querySelector("tabcustom");
+
+tabcustom.addEventListener("click", (e) => {});
